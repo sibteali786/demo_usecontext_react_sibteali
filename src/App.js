61c6955e215c821c837ef6edd1c9225f24eadd1d
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-05-29 10:30:20 +0500

    reverted to original react App styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
-import Parent from './Parent.js'
-import ValueContext from './ValueContext';
+
+
 function App() {
-  // let [number,setnumber] = useState(45);
-  let value = 81;
+  
+  
   return (
-    <ValueContext.Provider value={value}>
+    
 
     <div>Hello World
-      <Parent ></Parent>
-      <button >Number</button>
-      {/* Context 
-      Context hae a property which can then be used by all who are part of the context*/}
-      
+    
     </div> 
-    </ValueContext.Provider>
-  );
+      );
 }
 
 export default App;
--- src/ValueContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const ValueContext = React.createContext(45);
-export default ValueContext;
-// Context 
-/* It has two things 
-Provider 
-when we use context we provide so that component within that tree can access that value
-Consumer 
-we use consumer to consume the value   */
